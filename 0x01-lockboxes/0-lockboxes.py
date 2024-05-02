7a780c2dec1ff999a622e0a8cede1f6ad5e5d974
Mochatr <mohamedchatr8@gmail.com>
2024-05-02 12:14:35 -0700

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Define function """
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened 
+
+    Args:
+     boxes(list of lists of int): a list of lists.
+
+    Returns:
+     True if all bowes can be opened, False otherwise.
+    """
+    if not boxes:
+        return True
+
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+    queue = [0]
+
+    while queue:
+        current = queue.pop(0)
+        for key in boxes[current]:
+            if key < len(boxes) and not unlocked[key]:
+                unlocked[key] = True
+                queue.append(key)
+    return all(unlocked)
+
+if __name__ == "__main__":
+    boxes = [0], [1], [2], []]
+    print(canUnlockAll(boxes))
+
