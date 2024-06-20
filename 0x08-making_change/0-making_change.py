9f5624f2ae53a925f7f034a5136aa972b3d1b6f4
Mochatr <mohamedchatr8@gmail.com>
2024-06-20 15:07:52 -0700

    New version for efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""
-Module to solve the coin change problem.
-"""
+"""Define module"""
 
 
-def makeChange(coins, total):
-    """
-    Determine the fewest number of coins needed
-    to meet a given amount total.
+def makeChange(coin_denominations, amount):
+    """Make change"""
 
-    Param:
-        coins[list]: List of values of the coins in your possession
-        total[int]: Total amount to meet
-
-    Return:
-        Fewest number of coins needed to meet total
-    """
-    if total <= 0:
+    if amount <= 0:
         return 0
 
-    coins.sort(reverse=True)
+    num_coins_used, current_sum = 0, 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coin_denominations.sort(reverse=True)
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            if dp[i - coin] != float('inf'):
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+    for coin in coin_denominations:
+        while current_sum + coin <= amount:
+            num_coins_used += 1
+            current_sum += coin
 
-    return dp[total] if dp[total] != float('inf') else -1
+        if current_sum == amount:
+            return num_coins_used
+
+    return -1
