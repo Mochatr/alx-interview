0f5ef774c207dcc7be250a7184f50e5fd3a076db
Mochatr <mohamedchatr8@gmail.com>
2024-08-27 04:44:25 -0700

    Improving readability

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,23 +4,22 @@
 
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
-
     perimeter = 0
-    height = len(grid)
-    width = len(grid[0])
+    grid_height = len(grid)
+    grid_width = len(grid[0])
 
     for i, row in enumerate(grid):
         for j, is_land in enumerate(row):
             if is_land:
                 perimeter += 4
 
-                if (j + 1) < width and grid[i][j + 1]:
+                if (j + 1) < grid_width and grid[i][j + 1]:
                     perimeter -= 1
 
                 if (j - 1) >= 0 and grid[i][j - 1]:
                     perimeter -= 1
 
-                if (j + 1) < height and grid[i + 1][j]:
+                if (j + 1) < grid_height and grid[i + 1][j]:
                     perimeter -= 1
 
                 if (j - 1) >= 0 and grid[i - 1][j]:
